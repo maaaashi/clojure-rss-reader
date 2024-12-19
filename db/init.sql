6099cbc7dbef7e3b812c9b55924a0c867f589104
maaaashi <doyaseizin.7@gmail.com>
2024-12-19 23:27:08 +0900

    db

---
+++ db/init.sql
@@ -0,0 +1,25 @@
+-- テーブル作成
+create table feed (
+  id serial primary key,
+  url text not null,
+  title text not null,
+  description text not null,
+  created_at timestamp not null default current_timestamp
+);
+
+create table article (
+  id serial primary key,
+  feed_id integer not null references feed(id),
+  title text not null,
+  url text not null,
+  content text not null,
+  author text not null,
+  published_at timestamp not null,
+  created_at timestamp not null default current_timestamp
+);
+
+
+-- テストデータ挿入
+insert into feed (url, title, description) values
+('https://www.google.com', 'Google', 'Googleのトップページです。'),
+('https://www.yahoo.co.jp', 'Yahoo! JAPAN', 'Yahoo! JAPANのトップページです。');
--- src/clojure_rss_reader/handler/feeds.clj
+++ src/clojure_rss_reader/handler/feeds.clj
@@ -1,10 +1,15 @@
 (ns clojure-rss-reader.handler.feeds
   (:require [ataraxy.response :as response]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]))
 
-(defmethod ig/init-key ::get [_ _]
+
+(defmethod ig/init-key ::get [_ {{:keys [spec]} :db}]
   (fn [_]
-    [::response/ok {:message "OK"}]))
+    (let [result (jdbc/execute! spec ["SELECT * FROM feed"] {:builder-fn rs/as-unqualified-lower-maps})]
+      (println result)
+      [::response/ok {:message "OK"}])))
 
 (defmethod ig/init-key ::post [_ _]
   (fn [_]
